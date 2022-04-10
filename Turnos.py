922cc16f2cc3ae5c07c91e8e959b9d06549ccd65
P1514 <guinunogois@gmail.com>
2022-04-10 20:33:39 +0100

    New Feature More Complex Shifts Allowed

--- Turnos.py
+++ Turnos.py
@@ -23,27 +23,57 @@ import random
 import sys
 from time import sleep
 from os.path import exists
+import pandas as pd
 
-#inputs
-#n_oper = 11
-#n_auxprod=6
-#n_eng=5
-#n_control=3
-#n_arm=2
-#n_log=3
+@dataclass
+class Shift:
+    name: int
+    rests: int
+    workdays: int
+    stop_days: list
+    code: str
 
+
+    def __post_init__(self):
+        if self.workdays + len(self.stop_days) > 7:
+            raise NotImplementedError("Multiweek work schedule not supported")
+            
+@dataclass
+class People:
+    name: str
+    position: str
+    cur_shift: int
+    worklist: list = list
+
+
+    def __post_init__(self):
+        self.worklist = []
+
+@dataclass
+class Position:
+    name: str
+    shifts: list
+    min_people: int
+    required_people: int = 0
+
+
+#NOT USED YET
 efficiency = False
 
-positions = dict()
-positions["Operador"] = 9
-positions["Auxiliar de Produção"] = 5
-positions["Engenheiros"] = 4
-positions["Controlo"] = 2
-positions["Armazem"] = 1
-positions["Logistica"] = 2
+
+#Positions
+positions = list()
+positions.append(Position("Operador", [0,1,2], 9))
+positions.append(Position("Auxiliar de Produção",[0,1,2],5))
+positions.append(Position("Engenheiros",[0,1,2],4))
+positions.append(Position("Controlo",[0,1,2],2))
+positions.append(Position("Armazem",[0,1,2],1))
+positions.append(Position("Logistica",[0,1,2],2))
+#End Positions
+
 
 #WARNING: START DATE MUST BE A SUNDAY
-start_date = date(2022, 4, 3)
+start_date = date(2022, 4, 11)
 end_date = date(2022, 12, 31)
 
 #endinputs dont change anything else
@@ -51,150 +81,118 @@ end_date = date(2022, 12, 31)
 load_file=False
 
 
-result = {}
-@dataclass
-class Shift:
-    rests: int
-    workdays: int
-    stop_days: list
-
-
-    def __post_init__(self):
-        if self.workdays + len(self.stop_days) > 7:
-            raise NotImplementedError("Multiweek work schedule not supported")
-            
-
 #Shift Definition
 shifts = []
-shifts.append(Shift(1,6,[7]))
-shifts.append(Shift(1,6,[7]))
-shifts.append(Shift(0,5,[6,7]))
+shifts.append(Shift(0,1,6,[7], "M"))
+shifts.append(Shift(1, 1,6,[7], "T"))
+shifts.append(Shift(2, 0,5,[6,7], "N"))
 #End Shift Definition
 
 
-@dataclass
-class Pessoa:
-    turno: int
-    descanso: int
-
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
+def daterange(start_date, end_date, step = 7, start = 7):
+    for n in range(start, int((end_date - start_date).days),step):
         yield start_date + timedelta(n)
 
 def getweekday(single_date):
     return single_date.isocalendar()[2]
 
-def make_shift(n_ppl, nomes, grupo, posicao):
-    people = []
-    pre_result = []
-    for i in range(n_ppl):
-        people.append(Pessoa(grupo-1,0))
-        pre_result.append([posicao, str(grupo), str(nomes[i])])
+def assign_people_to_shift(workers, position):
+    for worker in workers:
+        worker.cur_shift = (worker.cur_shift+1)%len(position.shifts)
+
+def get_possible_rest_days(stop_days):
+    rest_days = list(range(1,8))
+    random.shuffle(rest_days)
+    for day in stop_days:
+        rest_days.remove(day)
+    return rest_days
+
+def get_current_shift_workers(workers, shift):
+    shift_workers = list()
+    for worker in workers:
+        if worker.cur_shift == shift:
+            shift_workers.append(worker)
+
+    return shift_workers
+
+def make_shift(workers, position):
     timeframe = daterange(start_date, end_date)
     for single_date in timeframe:
-        if getweekday(single_date) == 7:
-            pick_rest = list(range(1,7))
-            random.shuffle(pick_rest)
-            #reset vacation and change turn
-            for i in range(n_ppl):
-                if people[i].descanso:
-                    if len(pick_rest) != 0:
-                        leftover=pick_rest.pop() 
-                    else:
-                        pick_rest = list(range(1,7))
-                        random.shuffle(pick_rest)
-                        leftover=pick_rest.pop()
-                    if "D" in pre_result[i][-leftover]:
-                        old_leftover= leftover
-                        leftover=pick_rest.pop() if len(pick_rest) != 0 else random.randint(1,7)
-                        pick_rest.append(old_leftover)
-                    pre_result[i][-leftover] ="D"
-                
-                turn_code=(people[i].turno+1)%3
-                people[i].turno=turn_code
-                turn_code= "M" if turn_code == 0 else "T" if turn_code == 1 else "N"
-                people[i].descanso=1 if people[i].turno != 2 else 0
-                pre_result[i].append("D")
+        assign_people_to_shift(workers, position)
+        for shift in position.shifts:
+            shift = shifts[shift]
+            shift_code = shift.code
+            available_rests = get_possible_rest_days(shift.stop_days)
+            shift_workers = get_current_shift_workers(workers, shift.name)
+            for worker in shift_workers:
+                worklist = worker.worklist
+                worklist += [shift_code]*7
+                for stop_day in shift.stop_days:
+                    worklist[(-7+stop_day-1)]= "D"
 
-            pick_rest = list(range(n_ppl))
-            random.shuffle(pick_rest)
-        elif getweekday(single_date) == 6 and people[0].turno == 2:
-            for i in range(n_ppl):
-                pre_result[i].append("D")
-            continue
+                for i in range(shift.rests):
+                    if len(available_rests) == 0:
+                        available_rests = get_possible_rest_days(shift.stop_days)
+                    worklist[-7+available_rests.pop()-1] = "D"
 
-        else:
-            i=-1
-            if len(pick_rest) > 0 and people[0].turno != 2:
-                i = pick_rest.pop()
-                people[i].descanso = people[i].descanso-1
-                pre_result[i].append("D")
-            for ii in range(n_ppl):
-                if i != ii:
-                    pre_result[ii].append(turn_code)
 
-    return pre_result
-
-def make_shifts(nworkers, position):
-    pre_result = []
-    workers = [[],[],[]]
-    for nome, grupo in names[position]:
-        workers[grupo].append(nome)
-    for i in range(len(shifts)):
-        pre_result += make_shift(nworkers,workers[i], i, position)
-    
-    return pre_result
- 
 def required_people_per_shift(positions):
     fill_coefficient = 0
     for shift in shifts:
         fill_coefficient = max(fill_coefficient, shift.rests / shift.workdays)
-    for position, min_workers in positions.items():
+    for position in positions:
         #Missing Efficiency Here
-        positions[position] = min_workers + math.ceil(min_workers * fill_coefficient)
+        position.required_people = position.min_people + math.ceil(position.min_people * fill_coefficient)
 
 def make_people():
-    names = {}
-    for position, nworkers in positions.items():
-        names[position] = []
-        for grupo in range(len(shifts)):
-            for i in range(nworkers):
-                names[position].append([random.randint(0,100), grupo])
-    return names
+    people = {}
+    for position in positions:
+        people[position.name] = []
+        for shift in range(len(shifts)):
+            for i in range(position.required_people):
+                people[position.name].append(People(random.randint(0,100), position.name, shift))
+    return people
+
+
+#Read Excel Data
+open("")
+
+sys.exit(1)
+#Finish Read Excel Data
 
 
 required_people_per_shift(positions)
 
-if getweekday(start_date) != 7:
-    print("O PRIMEIRO DIA TEM DE SER UM DOMINGO")
+if getweekday(start_date) != 1:
+    print("O PRIMEIRO DIA TEM DE SER UMA Segunda")
     sleep(5)
     exit(1)
 
 
 
-timeframe = daterange(start_date, end_date)
+timeframe = daterange(start_date, end_date, 1, 0)
 #fill date
-result = "Posição,Grupo,Nome,"
-
-
+header = ["Posição","Nome"]
 
 for single_date in timeframe:
-        result+=str(single_date)+","
-result+="\n"
+    header.append(str(single_date))
 
-       
-#Make Operators
-pre_result = []
+people = make_people()
 
-names = make_people()
+for position in positions:
+    make_shift(people[position.name], position)
 
-for position, nworkers in positions.items():
-    pre_result += make_shifts(nworkers, position)
 
-for line in pre_result:
-    result+=",".join(line)+"\n"
+csv_result = ",".join(header)
+csv_result += "\n"
 
+for position, workers in people.items():
+    for worker in workers:
+        csv_result += f'{position},'
+        csv_result += f'{worker.name},'
+        csv_result += ",".join(worker.worklist)
+        csv_result += "\n"
 
 file_name = "turnos_novo.csv" if load_file else "turnos.csv"
 with open(file_name,"w") as file:
-    file.write(result)
+    file.write(csv_result)
